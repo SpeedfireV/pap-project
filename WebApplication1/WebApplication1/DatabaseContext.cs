5ef16f061bd8d9731213cc7533de606d21cf9d01
SpeedfireV <grzegorz.niespodzianek@gmail.com>
2025-11-29 16:08:31 +0100

    Added tables to db

--- WebApplication1/WebApplication1/DatabaseContext.cs
+++ WebApplication1/WebApplication1/DatabaseContext.cs
@@ -8,4 +8,11 @@ public class DatabaseContext: DbContext
     
     
     public DbSet<Error> Errors => Set<Error>();
+    public DbSet<Cargo> Cargos => Set<Cargo>();
+    public DbSet<Route> Routes => Set<Route>();
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+    public DbSet<Client> Clients => Set<Client>();
+    public DbSet<Job> Jobs => Set<Job>();
+    public DbSet<Transport> Transports => Set<Transport>();
+    public DbSet<Invoice> Invoices => Set<Invoice>();
 }
